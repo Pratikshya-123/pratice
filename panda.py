77c99229fe579804c3ab6ee5056e7dbe6d999e8b
Pratikshya-123 <64917247+Pratikshya-123@users.noreply.github.com>
2020-06-07 00:02:25 +0530

    Initial commit

---
+++ panda.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+data = pd.DataFrame({
+    'days':['mondays','tuesdays','wednesdays','thursdays','fridays','saturdays'],
+     'months':['jan','feb','mar','april','may','june']
+
+})
+
+print(data)
